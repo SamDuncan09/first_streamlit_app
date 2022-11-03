a5a6a0051419f249c7830efa12f8834a85adb87e
Sam Duncan <117125540+SamDuncan09@users.noreply.github.com>
2022-11-03 17:28:14 +0000

    Update streamlit_app.py
    -changed the url to seperate the fruit from the rest of the url to make it easier to change as a variable (line 28)

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 #takes the json and normalizes the data
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
